a6e02ca2659f3c30e3125c9d95fc654b854cf6ae
Rheysahm <conneyannor@gmail.com>
2026-02-24 05:04:25 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'ug-tcc-v2';
+const CACHE_NAME = 'ug-tcc-v2';
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './js/xlsx.full.min.js',
+  './js/exceljs.min.js',
+  './js/FileSaver.min.js',
+  './js/qrcode.min.js',
+  './js/html2canvas.min.js',
+  './js/jspdf.umd.min.js'
+];
+
+// Install Event - Caches the files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event - Serves from cache if offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
