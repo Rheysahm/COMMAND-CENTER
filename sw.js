b48147f5a7f63a74ca2ca5fa3138e22ee30ba618
Rheysahm <conneyannor@gmail.com>
2026-02-28 05:07:13 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,18 @@
-const CACHE_NAME = 'ug-tcc-v2';
-const CACHE_NAME = 'ug-tcc-v2';
+const CACHE_NAME = 'ug-tcc-app-v1';
+const ASSETS = [
   './',
   './index.html',
-  './style.css',
-  './app.js',
   './manifest.json',
-  './js/xlsx.full.min.js',
-  './js/exceljs.min.js',
-  './js/FileSaver.min.js',
-  './js/qrcode.min.js',
-  './js/html2canvas.min.js',
-  './js/jspdf.umd.min.js'
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.sheetjs.com/xlsx-latest/package/dist/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.3.0/exceljs.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
-// Install Event - Caches the files
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -22,7 +21,6 @@ self.addEventListener('install', (e) => {
   );
 });
 
-// Fetch Event - Serves from cache if offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
